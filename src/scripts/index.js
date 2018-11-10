6dfff911e51fb5be2b1acd4398817eb3b74c6eb5
Marcus Wilson <mswilson4040@hotmail.com>
2018-11-09 18:43:50 -0800

    triggering build

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,5 @@ test();
 async function test() {
     const http = new HttpClient();
     const result = await http.get('https://api.marcswilson.com/api/mlb/chadwick/counts');
-    console.log(result);
+    console.log('the result', result);
 }
