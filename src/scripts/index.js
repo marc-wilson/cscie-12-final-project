f8fe4ffc669936752dc3e05919a26379e575d220
Marcus Wilson <mswilson4040@hotmail.com>
2018-11-17 20:59:48 -0800

    added styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,26 @@
 
-
-async function test() {
-    const http = new HttpClient();
-    const result = await http.get('https://api.marcswilson.com/api/mlb/chadwick/counts');
-    console.log('the result', result);
+class Index {
+    constructor() {
+        this._httpClient = new HttpClient();
+        this.attendenceEl = document.getElementById('attendance');
+        this.ballparksEl = document.getElementById('ballparks');
+        this.peopleEl = document.getElementById('people');
+        this.teamsEl = document.getElementById('teams');
+        this.init();
+    }
+    async init() {
+        const counts = await this.getCounts();
+        this.attendenceEl.innerText = `Attendance Records: ${counts.attendance}`;
+        this.ballparksEl.innerText = `Ballparks: ${counts.ballparks}`;
+        this.peopleEl.innerText = `People (Players, Coaches): ${counts.people}`;
+        this.teamsEl.innerText = `Teams: ${counts.teams}`;
+        console.log(counts);
+    }
+    async getCounts() {
+        return await this._httpClient.get('https://api.marcswilson.com/api/mlb/chadwick/counts');
+    }
 }
+
+document.body.onload = (evt) => {
+    new Index();
+};
